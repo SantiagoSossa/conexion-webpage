56a5cb8df6738fe22ac4d1e91aad2abef8a78a5b
SantiagoSosssa <santiagosossa95@gmail.com>
2019-05-16 17:56:53 -0500

    feature - Experiences and images added, logic for experience filtering

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,13 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { bounce } from 'ng-animate';
 
 @Component({
   selector: 'experiences',
   templateUrl: './experiences.component.html',
-  styleUrls: ['./experiences.component.css']
+  styleUrls: ['./experiences.component.css'],
+  animations: [
+    trigger('bounce', [transition('* => *', useAnimation(bounce))])
+  ],
 })
 export class ExperiencesComponent implements OnInit {
 
+  showAll = true;
+  show1 = false;
+  show2 = false;
+  show3 = false;
+  bounce: any;
+
   constructor() { }
+
+  filterBy(filter){
+    this.resetButtons();
+    switch(filter){
+      case 'all':
+      this.showAll = true;
+      break;
+      case '1':
+      this.show1 = true;
+      break;
+      case '2':
+      this.show2 = true;
+      break;
+      case '3':
+      this.show3 = true;
+      break;
+    }
+  }
+
+  resetButtons(){
+  this.showAll = false;
+  this.show1 = false;
+  this.show2 = false;
+  this.show3 = false;
+  }
 
   ngOnInit() {
   }
