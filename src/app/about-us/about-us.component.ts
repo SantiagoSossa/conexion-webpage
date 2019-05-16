af9c5b9cb3871edddb681391dbd84485af25ba57
SantiagoSosssa <santiagosossa95@gmail.com>
2019-05-16 17:53:46 -0500

    feature - About us information cards created and CSS

--- src/app/about-us/about-us.component.ts
+++ src/app/about-us/about-us.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { slideInDown } from 'ng-animate';
 
 @Component({
   selector: 'about-us',
   templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.css']
+  styleUrls: ['./about-us.component.css'],
+  animations: [
+    trigger('slideInDown', [transition('* => *', useAnimation(slideInDown))])
+  ],
 })
 export class AboutUsComponent implements OnInit {
+
+  slideInDown: any;
 
   constructor() { }
 
