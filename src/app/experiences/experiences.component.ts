14ea1011d06c428fdc560fd6356e8b281893945d
SantiagoSosssa <santiagosossa95@gmail.com>
2019-05-22 13:57:53 -0500

    feature - Added new images for experiences, backgrounds, carousel and about us

--- src/app/experiences/experiences.component.ts
+++ src/app/experiences/experiences.component.ts
@@ -1,14 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, transition, useAnimation } from '@angular/animations';
-import { bounce } from 'ng-animate';
 
 @Component({
   selector: 'experiences',
   templateUrl: './experiences.component.html',
-  styleUrls: ['./experiences.component.css'],
-  animations: [
-    trigger('bounce', [transition('* => *', useAnimation(bounce))])
-  ],
+  styleUrls: ['./experiences.component.css']
 })
 export class ExperiencesComponent implements OnInit {
 
@@ -16,7 +11,38 @@ export class ExperiencesComponent implements OnInit {
   show1 = false;
   show2 = false;
   show3 = false;
-  bounce: any;
+  show4 = false;
+  show5 = false;
+  show6 = false;
+  show7 = false;
+  show8 = false;
+  customOptions: any = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    autoplay: true,
+    margin: 10,
+    autoWidth: true,
+    dots: false,
+    nav: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    }
+  }
 
   constructor() { }
 
@@ -35,6 +61,21 @@ export class ExperiencesComponent implements OnInit {
       case '3':
       this.show3 = true;
       break;
+      case '4':
+      this.show4 = true;
+      break;
+      case '5':
+      this.show5 = true;
+      break;
+      case '6':
+      this.show6 = true;
+      break;
+      case '7':
+      this.show7 = true;
+      break;
+      case '8':
+      this.show8 = true;
+      break;
     }
   }
 
@@ -43,6 +84,11 @@ export class ExperiencesComponent implements OnInit {
   this.show1 = false;
   this.show2 = false;
   this.show3 = false;
+  this.show4 = false;
+  this.show5 = false;
+  this.show6 = false;
+  this.show7 = false;
+  this.show8 = false;
   }
 
   ngOnInit() {
